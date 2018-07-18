b88b5f90fbbad64a10517c7aff72a7e6237aefb9
Mehret Kulala <merutaye@gmail.com>
2018-07-17 22:12:18 -0500

    getting actvities wip

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
+import activityRequests from '../../firebaseRequests/activity';
+
 import './Activity.css';
 
 class Activity extends React.Component {
+  state = {
+    activities: [],
+  }
+  
+  componentDidMount () {
+    activityRequests
+      .getRequest()
+      .then((activities) => {
+        this.setState({activities});
+      })
+      .catch((err) => {
+        console.error('error with activity get request', err);
+      });
+  }
+
   render () {
+    const activityComponents = this.state.activities.map((activity) => {
+      return (
+        <h2>{activity.activityName}</h2>
+      )
+    });
     return (
       <div className="Activity">
       <h1>Activity</h1>
+      {activityComponents}
       </div>
     );
   }
