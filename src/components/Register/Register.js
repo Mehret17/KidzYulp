b9fc3d8aaa36cf5ba9eae1df79064c7af5fbfa2f
Mehret Kulala <merutaye@gmail.com>
2018-07-17 19:40:25 -0500

    authentication for login works

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -18,7 +18,7 @@ class Register extends React.Component {
     authRequests
     .registerUser(user)
     .then(() => {
-      this.props.his.push('/mycollection');
+      this.props.history.push('/mycollection');
     })
     .catch((err) => {
       console.error('there was an error with register', err);
@@ -43,57 +43,56 @@ class Register extends React.Component {
       <div className="Register">
       <div id="login-form">
       <h1>Register</h1>
-      <form className="form-horizontal col-sm-4 col-sm-offset-4 col-xs-8 col-xs-8 col-xs-offset-2">
-      <div className="form-group">
-      <label htmlFor="inputEmail" className="col-sm-2 control-label">
-      Email 
-      </label>
-      <div className="col-sm-10">
-      <input
-      type="email"
-      className="form-control"
-      id="inputEmail"
-      placeholder="Email"
-      value={user.email}
-      onChange={this.emailChange}
-      />
+      <form className="form-horizontal col-sm-6 col-sm-offset-3">
+            <div className="form-group">
+              <label htmlFor="inputEmail" className="col-sm-4 control-label">
+                Email:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail"
+                  placeholder="Email"
+                  value={user.email}
+                  onChange={this.emailChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputPassword" className="col-sm-4 control-label">
+                Password:
+              </label>
+              <div className="col-sm-8">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputPassword"
+                  placeholder="Password"
+                  value={user.password}
+                  onChange={this.passwordChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-12 text-center">
+                <Link to="/login">Need to Login?</Link>
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-12">
+                <button
+                  type="submit"
+                  className="btn btn-default col-xs-12"
+                  onClick={this.registerClickEvent}
+                >
+                  Register
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
-      </div>
-      <div className="form-group">
-      <label htmlFor="inputPassword" className="col-sm-2 control-label">
-      Password
-      </label>
-      <div className="col-sm-10">
-      <input
-      type="password"
-      className="form-control"
-      id="inputPassword"
-      placeholder="Password"
-      value={user.password}
-      onChange={this.passwordChange}
-      />
-      </div>
-      </div>
-      <div className="form-group">
-      <div className="col-sm-offset-2 col-sm-10 text-center">
-      <Link to="/login">Neet to Login?</Link>
-      </div>
-      </div>
-      <div className="form-group">
-      <div className="col-sm-offset-2 col-sm-10">
-      <button
-      type="submit"
-      className="btn btn-default col-xs-12"
-      onClick={this.registerClickEvent}
-      >
-      Register
-      </button>
-      </div>
-      </div>
-      </form>
-      </div>
-      </div>
-
     );
   }
 }
