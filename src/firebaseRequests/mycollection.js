f58358a01317ebfa8c90314e045f38ce8c3b072f
Mehret Kulala <merutaye@gmail.com>
2018-07-21 14:21:35 -0500

    getting mycollection wip

--- src/firebaseRequests/mycollection.js
+++ src/firebaseRequests/mycollection.js
@@ -14,4 +14,24 @@ const postRequest = (myCollection) => {
   });
 };
 
-export default {postRequest}
+const getRequest = (uid) => {
+  return new Promise((resolve, reject) => {
+    axios
+     .post(`${constants.firebaseConfig.databaseURL}/myCollection.json?orderBy="uid"&equalTo="${uid}"`)
+     .then(res => {
+       const myCollections = [];
+       if (res.data !==null) {
+         Object.keys(res.data).forEach(fbKey => {
+           res.data[fbKey].id = fbKey;
+           myCollections.push(res.data[fbKey]);
+         });
+       }
+       resolve(myCollections);
+     })
+     .catch((err) => {
+       reject(err);
+     });
+  });
+};
+
+export default {postRequest, getRequest}
