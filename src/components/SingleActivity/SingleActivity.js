6a6c13ca0df4fcd3ed5952ab20016038f071d30e
Mehret Kulala <merutaye@gmail.com>
2018-07-24 21:32:14 -0500

    updating on firebase works

--- src/components/SingleActivity/SingleActivity.js
+++ src/components/SingleActivity/SingleActivity.js
@@ -2,24 +2,51 @@ import React from 'react';
 
 import './SingleActivity.css';
 
+// const defaultActivity = {
+//   name: '',
+//   imgUrl: '',
+//   activityUrl: '',
+//   time: '',
+//   address: '',
+//   type: '',
+//   theme: '',
+//   description: '',
+//   comment: '',
+// };
+
 class SingleActivity extends React.Component {
   state = {
     isClicked: false,
+    comment: {
+      text: '',
+    }
   }
   
+  textFieldStringState = (e) => {
+    const tempActivity = {...this.state.comment};
+    tempActivity.text = e.target.value;
+    this.setState({comment: tempActivity});
+  };
+
   render() {
+    const {comment} = this.state;
     const {saved, details } = this.props;
     const addCollection = (e) => {
       this.props.saveActivity(this.props.details)
     };
 
-    const deleteCollection = (e) => {
-      this.props.onClick(this.props.details.id)
-    };
-
     const showForm = (e) => {
       this.setState({isClicked: !this.state.isClicked})
     };
+
+    const deleteCollection = (e) => {
+      this.props.onClick(this.props.details.id)
+    
+    };
+
+    const updatedCollection = () => {
+      this.props.onSubmit(this.props.details.id, this.state.comment)
+    }
 
     return (
       <div className="container">
@@ -33,7 +60,7 @@ class SingleActivity extends React.Component {
             <p className="theme">{details.theme}</p>
             <p className="description">{details.description}</p>
           {
-              saved ? (
+            saved ? (
                 <div className="text-center">
                 <button className="btn btn-primary"onClick={showForm}>Update</button>
                 <button className="btn btn-danger"onClick={deleteCollection}>Delete</button>
@@ -44,6 +71,24 @@ class SingleActivity extends React.Component {
                 </div>
               )
             }
+
+            {
+              this.state.isClicked ? (
+                <div className="row">
+                <fieldset>
+                  <textarea className="col-xs-12"
+                    type="text"
+                    id="name"
+                    placeholder="Note"
+                    value={comment.text}
+                    onChange={this.textFieldStringState}
+                  />
+                  <button className="update"onClick={updatedCollection}>Submit</button>
+                </fieldset>
+                </div>
+              ) : null 
+            }
+
           </div>
         </div>
       </div>
