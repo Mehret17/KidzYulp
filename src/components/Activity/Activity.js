4520b9557bb5b1b721c9d83e2fcaea5bb2b39bbd
Mehret Kulala <merutaye@gmail.com>
2018-07-30 21:23:01 -0500

    styling wip

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -15,27 +15,28 @@ class Activity extends React.Component {
   state = {
     activities: [],
     mycollection: {},
+
   }
 
   saveActivity = (activities) => {
-    const collection = {...this.state.mycollection};
-      collection.name = activities.name;
-      collection.imgUrl = activities.imgUrl;
-      collection.activityUrl = activities.activityUrl;
-      collection.time = activities.time; 
-      collection.address = activities.address; 
-      collection.type = activities.type; 
-      collection.theme = activities.theme; 
-      collection.description = activities.description;
-      collection.uid = authRequests.getUid(); 
-      myCollectionRequests
-        .postRequest(collection)
-        .then(() => {
-          this.props.history.push('/mycollection');
-        })
-        .catch((err) => {
-          console.error('error in my collection post', err)
-        })
+    const collection = { ...this.state.mycollection };
+    collection.name = activities.name;
+    collection.imgUrl = activities.imgUrl;
+    collection.activityUrl = activities.activityUrl;
+    collection.time = activities.time;
+    collection.address = activities.address;
+    collection.type = activities.type;
+    collection.theme = activities.theme;
+    collection.description = activities.description;
+    collection.uid = authRequests.getUid();
+    myCollectionRequests
+      .postRequest(collection)
+      .then(() => {
+        this.props.history.push('/mycollection');
+      })
+      .catch((err) => {
+        console.error('error in my collection post', err)
+      })
   };
 
   formSubmitEvent = (newActivity) => {
@@ -52,14 +53,19 @@ class Activity extends React.Component {
       .getRequest()
       .then((activities) => {
         newActivityRequests.getNewActivity()
-        .then((newactivities) => {
-        this.setState({ activities:[...activities,...newactivities] });
+          .then((newactivities) => {
+            this.setState({ activities: [...activities, ...newactivities] });
+          })
       })
-    })
       .catch((err) => {
         console.error('error with activity get request', err);
       });
   }
+
+  // onChange = (e) => {
+  //   this.setState({search: e.target.value });
+  // };
+
   render() {
     const activityComponents = this.state.activities.map((activity) => {
       return (
@@ -69,14 +75,13 @@ class Activity extends React.Component {
           saveActivity={this.saveActivity}
         />
       )
-      
     });
     return (
       <div className="header">
         <h1>Activity</h1>
         <div className="mainBody">
           <button>
-            <Link to={{pathname:"/addnewactivity",onSubmit:this.formSubmitEvent}}>Add New Activity</Link></button>
+            <Link to={{ pathname: "/addnewactivity", onSubmit: this.formSubmitEvent }}>Add New Activity</Link></button>
           {activityComponents}
         </div>
       </div>
--- src/components/SingleActivity/SingleActivity.js
+++ src/components/SingleActivity/SingleActivity.js
@@ -13,29 +13,29 @@ class SingleActivity extends React.Component {
   }
 
   onStarClick(nextValue, prevValue, name) {
-    this.setState({rating: nextValue});
+    this.setState({ rating: nextValue });
   }
-  
+
   textFieldStringState = (e) => {
-    const tempActivity = {...this.state.comment};
+    const tempActivity = { ...this.state.comment };
     tempActivity.text = e.target.value;
-    this.setState({comment: tempActivity});
+    this.setState({ comment: tempActivity });
   };
 
   render() {
-    const {comment, rating} = this.state;
-    const {saved, details } = this.props;
+    const { comment, rating } = this.state;
+    const { saved, details } = this.props;
     const addCollection = (e) => {
       this.props.saveActivity(this.props.details)
     };
 
     const showForm = (e) => {
-      this.setState({isClicked: !this.state.isClicked})
+      this.setState({ isClicked: !this.state.isClicked })
     };
 
     const deleteCollection = (e) => {
       this.props.onClick(this.props.details.id)
-    
+
     };
 
     const updatedCollection = () => {
@@ -44,57 +44,63 @@ class SingleActivity extends React.Component {
 
     return (
       <div className="container">
+      <div className="row">
         <div className="col-sm-3">
-          <img src={details.imgUrl} alt={details.imgUrl}/>
-          <div className="SingleActivity">
-            <h3 className="name">{details.name}</h3>
-            <p className="time">{details.time}</p>
-            <p className="address">{details.address}</p>
-            <p className="type">{details.type}</p>
-            <p className="theme">{details.theme}</p>
-            <p className="description">{details.description}</p>
-          
-          {
-            comment ? (
-              <p className="comment">{details.comment}</p>
-            ) : null
-          }
-          
-          {
-            saved ? (
-                <div className="text-center">
-                <button className="btn btn-primary"onClick={showForm}>Update</button>
-                <button className="btn btn-danger"onClick={deleteCollection}>Delete</button>
-                </div>
-              ) : (
-                <div className="text-center">
-                <StarRating name="rate" starCount={5} value={rating} onStarClick={this.onStarClick.bind(this)}/>
-                <button className="btn btn-primary"onClick={addCollection}>Save</button>
-                </div>
-              )
-            }
-
-            {
-              this.state.isClicked ? (
-                <div className="row">
-                <fieldset>
-                  <textarea className="col-xs-12"
-                    type="text"
-                    id="name"
-                    placeholder="Note"
-                    value={comment.text}
-                    onChange={this.textFieldStringState}
-                  />
-                  <button className="update"onClick={updatedCollection}>Submit</button>
-                  <button className="Cancel"onClick={showForm}>X</button>
-                </fieldset>
-                </div>
-              ) : null 
-            }
-
-          </div>
+          <div className="thumbnail">
+            {/* <div className="col-sm-3"> */}
+            <img src={details.imgUrl} alt={details.imgUrl} />
+           </div>
         </div>
-      </div>
+            <div className="SingleActivity col-sm-3">
+            <div className="thumbnail">
+              <h3 className="name">{details.name}</h3>
+              <p className="time">{details.time}</p>
+              <p className="address">{details.address}</p>
+              <p className="type">{details.type}</p>
+              <p className="theme">{details.theme}</p>
+              <p className="description">{details.description}</p>
+
+              {
+                comment ? (
+                  <p className="comment">{details.comment}</p>
+                ) : null
+              }
+
+              {
+                saved ? (
+                  <div className="text-center">
+                    <button className="btn btn-primary" onClick={showForm}>Update</button>
+                    <button className="btn btn-danger" onClick={deleteCollection}>Delete</button>
+                  </div>
+                ) : (
+                  <div className="text-center">
+                  <StarRating name="rate" starCount={5} value={rating} onStarClick={this.onStarClick.bind(this)} />
+                     <button className="btn btn-primary" onClick={addCollection}>Save</button>
+                  </div>
+                  )
+              }
+             </div>
+             </div>
+              {
+                this.state.isClicked ? (
+                  <div className="row">
+                    <fieldset>
+                      <textarea className="col-xs-12"
+                        type="text"
+                        id="name"
+                        placeholder="Note"
+                        value={comment.text}
+                        onChange={this.textFieldStringState}
+                      />
+                      <button className="update" onClick={updatedCollection}>Submit</button>
+                      <button className="Cancel" onClick={showForm}>X</button>
+                    </fieldset>
+                  </div>
+                ) : null
+              }    
+          </div>
+       </div>
+      // </div>
     );
   }
 }
